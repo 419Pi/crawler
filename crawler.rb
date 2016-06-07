7575e2970068877c1a47a4a71c4f9b723089c143
Andrew Gremmo <andrewjgremmo@gmail.com>
2016-06-06 22:19:18 -0400

    Readd keywords and limit

--- crawler.rb
+++ crawler.rb
@@ -4,21 +4,24 @@ require_relative('node.rb')
 class Crawler
   attr_reader :visited, :root
 
-  def initialize(url)
+  def initialize(url, limit, keyword)
     @root = Node.new(nil, url)
     @seen = {url => true}
+    @limit = limit.to_i
+    @keyword = keyword
     @queue = [@root]
   end
 
   def crawl
-    while @seen.length < 100 && !@queue.empty?
+    while @seen.length < @limit && !@queue.empty?
+      puts @seen.length
       node = dequeue
       parse(node) unless node.nil?
     end
   end
 
   def enqueue(parent, url)
-    return if @seen[url]
+    return if @seen[url] || @seen.length > @limit
     @seen[url] = true
     node = Node.new(parent, url)
     @queue.unshift(node)
@@ -29,7 +32,14 @@ class Crawler
     begin
       url = node.url
       puts "crawling #{url}"
-      response = HTTParty.get(url)
+      response = HTTParty.get(url).force_encoding("ISO-8859-1").encode!("UTF-8")
+      if @keyword
+        if response.match(/(^|\s+)#{@keyword}(\s+|$|[\p{P}\p{S}])/i)
+          node.keyword = true
+          @queue = []
+          return
+        end
+      end
       response.scan(/<a.+?href="(.+?)"/).each do |link|
         link = link.join('').chomp('/')
         next if !link.match(/^\/?[a-zA-Z]+/)
